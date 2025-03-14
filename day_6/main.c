edf0d9d7beb38d46ccbdfbe050d872fbe3a16bc7
Toohk <toohk3@gmail.com>
2025-03-14 09:50:33 +0100

    -

--- day_6/main.c
+++ day_6/main.c
@@ -21,45 +21,32 @@ void display_map(char map[lx][ly]){
     printf("\n\n\n");
 }
 
+void clean_map(char map[lx][ly]){
+    for(int x = 0; x<lx; x++){
+        for(int y = 0; y<ly; y++){
+            if(map[x][y]== 'X' || map[x][y] == '+') map[x][y] = '.';
+        }
+    }
+}
+
 int run_sequence(char map[lx][ly], vec2 pos){
+    int vecx[]={-1,0,1,0},vecy[]={0,1,0,-1},veci=0;
     bool moving = true;
     int loop_score = 0;
     while (moving && loop_score < 4) {
         loop_score = 0;
+        
+        while(map[pos.x+vecx[veci]][pos.y+vecy[veci]] == '#')veci=(veci+1) % 4;
 
-        if(map[pos.x-1][pos.y]== 'X') loop_score++;
-        while (map[pos.x-1][pos.y] != '#') {
-            if(pos.x -1 < 0){ moving = false; break;}
-            pos.x = pos.x -1;
-            map[pos.x][pos.y] = 'X';
+        if(pos.x+vecx[veci] <= 0 || pos.x+vecx[veci] >= lx-1 || pos.y+vecy[veci] <= 0 || pos.y+vecy[veci] >= ly-1 ){ 
+            moving = false; 
         }
-        if(!moving) break;
+        pos.x+=vecx[veci];
+        pos.y+=vecy[veci];
 
-        if(map[pos.x][pos.y+1]== 'X') loop_score++;
-        while (map[pos.x][pos.y+1] != '#') {
-            if(pos.y +1 > ly -1){ moving = false; break;}
-            pos.y = pos.y +1;
-            map[pos.x][pos.y] = 'X';
-        }
-        if(!moving) break;
-
-        if(map[pos.x+1][pos.y]== 'X') loop_score++;
-        while (map[pos.x+1][pos.y] != '#') {
-            if(pos.x +1 > lx -1){ moving = false; break;}
-            pos.x = pos.x +1;
-            map[pos.x][pos.y] = 'X';
-        }
-        if(!moving) break;
-
-        if(map[pos.x][pos.y-1]== 'X') loop_score++;
-        while (map[pos.x][pos.y-1] != '#') {
-            if(pos.y -1 < 0){ moving = false; break;}
-            pos.y = pos.y -1;
-            map[pos.x][pos.y] = 'X';
-        }
-        if(!moving) break;
-
-        //if(loop_score > 3) printf("alert\n");
+        if(map[pos.x][pos.y] != '+') map[pos.x][pos.y] = 'X';
+        if(map[pos.x][pos.y] == '+' && map[pos.x][pos.y+1] != '.') loop_score++;
+        map[pos.x][pos.y] = '+';
     }
     if(loop_score >=4) return 1;
     return 0;
@@ -70,35 +57,26 @@ void part_two(char map[lx][ly], vec2 pos){
     char new_map [lx][ly];
     int n_loop = 0;
     memcpy(new_map, map, lx * ly * sizeof(char));
-    new_map[4][8] = '#';
-    run_sequence(new_map, pos);
-    display_map(new_map);
-/*
     for(int i = 0; i<lx; i++){
         for(int j = 0; j<ly; j++){
-            if(pos.x == i && pos.y == j) continue;
+            if(map[i][j] != 'X' && map[i][j] != '+') continue;
             memcpy(new_map, map, lx * ly * sizeof(char));
+            clean_map(new_map); 
             new_map[i][j] = '#';
-            printf("%d %d\n", i,j);
             n_loop = n_loop + run_sequence(new_map,pos);
         }
     }
-*/
     printf("Part two result : %d \n", n_loop);
 }
 
 void part_one(char map[lx][ly], vec2 pos){
-
     run_sequence(map, pos);
-    display_map(map);
-
     int n_pos = 0;
     for(int i = 0; i<lx; i++){
         for(int j = 0; j<ly; j++){
-            if(map[i][j] == 'X') n_pos++;
+            if(map[i][j] == 'X' || map[i][j] == '+') n_pos++;
         }
     }
-
     printf("Part one result : %d \n", n_pos);
 }
 
@@ -123,7 +101,7 @@ int main(){
 
     fclose(file);
 
-    //part_one(map, guard_pos);
+    part_one(map, guard_pos);
     part_two(map, guard_pos);
 
     return 0;
